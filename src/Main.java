deb3ae77ef3de1666ad9fa24c1f81c3cbed4fe50
Lars L. <lars.langmack@gmail.com>
2025-10-22 20:00:24 +0200

    Hello World Program Update

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    //Hello-World program shown in lecture, 22.10.2025
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
